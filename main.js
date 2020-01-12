ffee459be378e61177d1cd8b2bfde0023c2bfaf3
hbrashid <hbrashid@gmail.com>
2020-01-12 16:58:26 -0600

    RPS Project

--- main.js
+++ main.js
@@ -15,6 +15,33 @@ const rl = readline.createInterface({
 const rockPaperScissors = (hand1, hand2) => {
 
   // Write code here
+
+  hand1 = hand1.trim().toLowerCase();
+  hand2 = hand2.trim().toLowerCase();
+
+  if (hand1 === hand2) {
+    return "It's a tie!";
+  }
+  else if (hand1 === 'rock' && hand2 === 'paper') {
+    return "Hand two wins!";
+  }
+  else if (hand1 === 'paper' && hand2 === 'scissors') {
+    return "Hand two wins!";
+  }
+  else if (hand1 === 'rock' && hand2 === 'scissors') {
+    return "Hand one wins!";
+  }
+  else if (hand1 === 'scissors' && hand2 === 'paper') {
+    return "Hand one wins!";
+  }
+  else if (hand1 === 'paper' && hand2 === 'rock') {
+    return "Hand one wins!";
+  }
+  else if (hand1 === 'scissors' && hand2 === 'rock') {
+    return "Hand two wins!";
+  }
+
+
   // Use the unit test to see what is expected
 
 }
